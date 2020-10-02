6a7a34c4957e62008275eb7e4bebbfe854829c83
MegaSkyWars <rafaellourenco2011@hotmail.com>
2020-10-02 09:11:20 +0100

    Addec comments

--- Project 1/js/code.js
+++ Project 1/js/code.js
@@ -1,24 +1,44 @@
 
+/**
+ * Creates the whole Structure
+ */
 function createStructure() {
     'user strict';
 }
 
+/**
+ * Called when the window is resized
+ */
 function onResize() {
     'use strict';
 }
 
+/**
+ * Called when a key is pressed
+ * @param {*} e 
+ * Info about the pressed key
+ */
 function onKeyDown(e) {
     'use strict';
 }
 
+/**
+ * Renders the whole Structure
+ */
 function render() {
     'use strict';
 }
 
+/**
+ * Called at the beggining of the program
+ */
 function init() {
     'use strict';
 }
 
+/**
+ * Updates the locations and rotations of the Structure
+ */
 function animate() {
     'use strict';
 }
