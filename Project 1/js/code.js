b30bbf064356484ae1cd4bae6d55f73b4e3615f9
MegaSkyWars <rafaellourenco2011@hotmail.com>
2020-10-02 09:09:07 +0100

    Added main empty functions

---
+++ Project 1/js/code.js
@@ -0,0 +1,24 @@
+
+function createStructure() {
+    'user strict';
+}
+
+function onResize() {
+    'use strict';
+}
+
+function onKeyDown(e) {
+    'use strict';
+}
+
+function render() {
+    'use strict';
+}
+
+function init() {
+    'use strict';
+}
+
+function animate() {
+    'use strict';
+}
