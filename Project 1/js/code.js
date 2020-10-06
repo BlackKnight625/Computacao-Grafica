525ce3e9e6889adc1b8a0d0f653f2166d6e5cf56
miguel-lourenco <miguel.lourenco@hotmail.com>
2020-10-06 20:24:34 +0100

    added a skeleton to code.js

--- Project 1/js/code.js
+++ Project 1/js/code.js
@@ -1,3 +1,4 @@
+var renderer, scene, camera;
 
 function createStructure() {
     'user strict';
@@ -13,10 +14,29 @@ function onKeyDown(e) {
 
 function render() {
     'use strict';
+    renderer.render(scene, camera);
 }
 
 function init() {
-    'use strict';
+    'use strict'
+    scene = new THREE.Scene();
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(new THREE.Color(0xEEEEEE));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.x = 30;
+    camera.position.y = 30;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+
+    var axes = new THREE.AxisHelper(20);
+    scene.add(axes);
+
+    document.body.appendChild(renderer.domElement);
+
+    render();
 }
 
 function animate() {
