046fdf8fdeaf1460b7379c91174b3ac366f1f1d7
miguel-lourenco <miguel.lourenco@hotmail.com>
2020-10-10 21:05:08 +0100

    reverted changes

--- Project 1/js/code.js
+++ Project 1/js/code.js
@@ -2,33 +2,11 @@ var renderer, scene, camera;
 var g0, g1, g2;
 var hasteMaterial = new THREE.MeshBasicMaterial({color: 0x606060});
 
-function createSet0() {
-    g0 = new THREE.Object3D();
-    var mouthMaterial = new THREE.MeshBasicMaterial({color: 0xFF0000});
-    var mouth = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 3, 30), mouthMaterial);
-    mouth.rotation.x = Math.PI * 0.5; 
-    mouth.position.set(-7.5, -30, 0);
-    g0.add(mouth);
-
-    var haste = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 30, 30), hasteMaterial);
-    haste.position.set(-10, -15, 0);
-    g0.add(haste);
-
-    haste = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 30, 30), hasteMaterial);
-    haste.rotation.z = Math.PI * 0.5;
-    haste.position.set(-10, 0, 0);
-    g0.add(haste);
-}
-
 /**
  * Creates the whole Structure
  */
 function createStructure() {
     'user strict';
-
-    createSet0();
-
-    //scene.add(g0);
 
     g1 = new THREE.Object3D();
 
@@ -39,29 +17,7 @@ function createStructure() {
 
 
     haste = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 20, 30), hasteMaterial);
-    haste.rotation.z = Math.PI * -(1/3);
-    haste.position.set(-8+10, 0, 0);
-    g1.add(haste);
-
-    haste = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 25, 30), hasteMaterial);
-    haste.rotation.z = Math.PI * (1/3);
-    haste.position.set(8+13, 0, 0);
-    g1.add(haste);
-
-    haste = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 20, 30), hasteMaterial);
-    haste.position.set(-2+10, -6, 0);
-    g1.add(haste);
-
-
-    //haste fora do sitio (base do nariz)
-    haste = new THREE.Mesh(new THREE.CylinderGeometry(1, 1, 20, 30), hasteMaterial);
     haste.position.set(0, 0, 0);
-    g1.add(haste);
-
-    g0.position.set(0+10, -16, 0);
-    g1.add(g0);
-
-    scene.add(g1);
 }
 
 /**
@@ -85,7 +41,6 @@ function onKeyDown(e) {
  */
 function render() {
     'use strict';
-    //g1.rotation.y += 0.02;
 
     requestAnimationFrame(render);
     renderer.render(scene, camera);
