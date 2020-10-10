61ef533ea98acf1e306ba89f0df55ae38b57ef19
mafmagalhaes19 <mafi.magalhaes@gmail.com>
2020-10-10 23:13:41 +0100

    it has fucking eyes

--- Project 1/js/code.js
+++ Project 1/js/code.js
@@ -3,6 +3,48 @@ var g0, g1, g2;
 var hasteMaterial = new THREE.MeshBasicMaterial({color: 0x606060});
 var clock = new THREE.clock();
 
+function createEye() {
+    var eye_set = new THREE.Object3D();
+
+    var eyeMaterial_base = new THREE.MeshBasicMaterial({color: 0xFFFDFD});
+    
+    var left_eye_base = new THREE.Mesh (new THREE.CylinderGeometry(0.8, 0.8, 0.8, 30), eyeMaterial_base);
+    left_eye_base.rotation.x = Math.PI * 0.5;
+    left_eye_base.position.set(-2,-0.75,0);
+    eye_set.add(left_eye_base);
+
+    var right_eye_base = new THREE.Mesh (new THREE.CylinderGeometry(0.8, 0.8, 0.8, 30), eyeMaterial_base);
+    right_eye_base.rotation.x = Math.PI * 0.5;
+    right_eye_base.position.set(2,-0.75,0);
+    eye_set.add(right_eye_base);
+
+    var eyeMaterial_pupil = new THREE.MeshBasicMaterial({color: 0x000000});
+
+    var left_eye_pupil = new THREE.Mesh (new THREE.CylinderGeometry(0.5, 0.5, 0.5, 30), eyeMaterial_pupil);
+    left_eye_pupil.rotation.x = Math.PI * 0.5;
+    left_eye_pupil.position.set(-2,-0.75,1);
+    eye_set.add(left_eye_pupil);
+
+    var right_eye_pupil = new THREE.Mesh (new THREE.CylinderGeometry(0.5, 0.5, 0.5, 30), eyeMaterial_pupil);
+    right_eye_pupil.rotation.x = Math.PI * 0.5;
+    right_eye_pupil.position.set(2,-0.75,1);
+    eye_set.add(right_eye_pupil);
+
+    var eyeMaterial_iris = new THREE.MeshBasicMaterial({color: 0x2FE3F7});
+
+    var left_eye_iris = new THREE.Mesh (new THREE.CylinderGeometry(0.2, 0.2, 0.2, 30), eyeMaterial_iris);
+    left_eye_iris.rotation.x = Math.PI * 0.5;
+    left_eye_iris.position.set(-2,-0.75,2);
+    eye_set.add(left_eye_iris);
+
+    var right_eye_iris = new THREE.Mesh (new THREE.CylinderGeometry(0.2, 0.2, 0.2, 30), eyeMaterial_iris);
+    right_eye_iris.rotation.x = Math.PI * 0.5;
+    right_eye_iris.position.set(2,-0.75,2);
+    eye_set.add(right_eye_iris);
+
+    return eye_set;
+}
+
 function createNose() {
     var nose_set = new THREE.Object3D();
 
@@ -12,6 +54,7 @@ function createNose() {
     nose_set.add(nose);
 
     var nostrilMaterial = new THREE.MeshBasicMaterial({color: 0x994C00});
+
     var left_nostril = new THREE.Mesh(new THREE.CylinderGeometry(0.2, 0.2, 3, 30), nostrilMaterial);
     left_nostril.rotation.x = Math.PI * 0.5;
     left_nostril.position.set(-0.25, -0.25, 0);
@@ -34,7 +77,9 @@ function createStructure() {
     g1 = new THREE.Object3D();
 
     var nose_set = createNose();
+    var eye_set = createEye();
     scene.add(nose_set);
+    scene.add(eye_set);
 
     haste = new THREE.Mesh(new THREE.CylinderGeometry(0.25, 0.25, 0.75, 30), hasteMaterial);
     haste.rotation.rotation
